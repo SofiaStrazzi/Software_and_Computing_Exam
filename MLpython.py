faf92dd08fdef3a5784ea77906534a81d65af5cf
Sofia Strazzi <61978347+SofiaStrazzi@users.noreply.github.com>
2021-08-25 17:42:12 +0200

    Update MLpython.py
    Heading of the file that explains what is written inside it

--- MLpython.py
+++ MLpython.py
@@ -1,1 +1,11 @@
+# Program to find events containing Higgs bosons decaying to b quark pairs in the LHC data
 
+# Three different datasets are used to test the program:
+#    - Higgs bosons (higgsevents.root) which contains the signal events
+#    - Top quark pairs (ttbarevents.root) which contains background events
+#    - Data (dataevents.root) containing both signal and background events that I want to analyze
+
+# The three datasets are extracted from files downloaded by Delphes portal:
+# they have been cutted through the macro copyleaves in order to obtain smaller files with just the information that I needed
+
+# All the trees are called EventsTree 
