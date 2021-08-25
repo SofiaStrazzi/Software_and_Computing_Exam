30d612f760e2e93fe21e43d94f49e3336134c319
Sofia Strazzi <61978347+SofiaStrazzi@users.noreply.github.com>
2021-08-25 18:50:50 +0200

    Update MLpython.py
    I import the libraries that I think I need: tensorflow, pandas, uproot (usually not present,  so I put the command in the file) and bumpy

--- MLpython.py
+++ MLpython.py
@@ -9,3 +9,16 @@
 # they have been cutted through the macro copyleaves in order to obtain smaller files with just the information that I needed
 
 # All the trees are called EventsTree 
+
+import tensorflow as tf
+import pandas as pd
+!pip3 install uproot
+import uproot
+import numpy as np
+from matplotlib import pyplot as plt
+from sklearn.metrics import roc_curve
+from IPython.display import clear_output
+clear_output()
+
+# Print the version of TensorFlow
+print("TensorFlow " + str(tf.__version__))
