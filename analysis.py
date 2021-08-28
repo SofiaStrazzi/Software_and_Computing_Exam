979cf8b17fe01c7ad4804cdb4098b6a375069e1c
Sofia Strazzi <61978347+SofiaStrazzi@users.noreply.github.com>
2021-08-28 09:42:28 +0200

    Update analysis.py
    Question for the user

--- analysis.py
+++ analysis.py
@@ -1,1 +1,6 @@
+# The user can decide if using an analysis based on PYTHON libraries or based on ROOT TMVA
+
+print("Press 1 to analyze data with PYTHON libraries or 2 to analyze them with TMVA \t")
+type_of_analysis = raw_input()
+
 
