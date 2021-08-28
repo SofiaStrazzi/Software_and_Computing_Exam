6a8bae07085d33b1441bfe54561d1037879ae31c
Sofia Strazzi <61978347+SofiaStrazzi@users.noreply.github.com>
2021-08-28 09:51:26 +0200

    Update MLtmva.C
    I added some libraries that surely I will need, maybe other will be requested

--- MLtmva.C
+++ MLtmva.C
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <map>
 
+#include "TMVA/TMVAGui.h"
+#include "TMVA/DataLoader.h"
+#include "TMVA/Factory.h"
+#include "TMVA/Tools.h"
+
+#include "TFile.h"
+#include "TTree.h"
+#include "TString.h"
+#include "TObjString.h"
+#include "TSystem.h"
+#include "TROOT.h"
+#include "TChain.h"
