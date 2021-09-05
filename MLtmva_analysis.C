177de36fbbdf2baa12de538c03000204e35c23d6
Sofia Strazzi <61978347+SofiaStrazzi@users.noreply.github.com>
2021-09-05 22:36:38 +0200

    Update MLtmva_analysis.C
    inclusions

--- MLtmva_analysis.C
+++ MLtmva_analysis.C
@@ -1,1 +1,26 @@
+ // This macro analyse the data with the method chosen by the user
+   // Cut optimisation (CutsGA)
+   // Linear Discriminant Analysis (Fisher discriminant)
+   // Neural Networks MLPBNN (feed-forward Multilayer Perceptrons
+   // Boosted Decision Trees (Adaptive Boost) 
 
+
+#include <cstdlib>
+#include <vector>
+#include <iostream>
+#include <map>
+#include <string>
+ 
+#include "TFile.h"
+#include "TTree.h"
+#include "TString.h"
+#include "TSystem.h"
+#include "TROOT.h"
+#include "TStopwatch.h"
+ 
+#include "TMVA/Tools.h"
+#include "TMVA/Reader.h"
+#include "TMVA/MethodCuts.h"
+ 
+using namespace TMVA;
+using namespace std;
