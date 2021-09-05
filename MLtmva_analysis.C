518119ffb5403745079fafcf39c257638278efe3
Sofia Strazzi <61978347+SofiaStrazzi@users.noreply.github.com>
2021-09-05 22:42:04 +0200

    Update MLtmva_analysis.C
    Introduction of the function

--- MLtmva_analysis.C
+++ MLtmva_analysis.C
@@ -24,3 +24,26 @@
  
 using namespace TMVA;
 using namespace std;
+ 
+void MLtmva_analysis()
+{
+   // Library loading
+   TMVA::Tools::Instance();
+ 
+   // Default MVA methods to be trained + tested
+   std::map<std::string,int> Use;
+ 
+
+   // I ask to the user to select the chosen method to analyse the data
+   int_t method;
+
+   cout << "\n\nPRESS 1, 2, 3 OR 4 TO SELECT THE METHOD THAT YOU WANT TO USE TO ANALYZE THE DATA:" << endl << endl;
+
+   cout << "Press 1, 2, 3 or 4:" << endl;
+
+   cout << "1 Cut optimisation (CutsGA)" << endl
+   		<< "2 Linear Discriminant Analysis (Fisher discriminant)" << endl
+   		<< "3 Neural Networks MLPBNN (feed-forward Multilayer Perceptrons" << endl
+   		<< "4 Boosted Decision Trees (Adaptive Boost) " << endl;
+
+   cin >> method;
