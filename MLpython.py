f26d48a9c657730981d6b11143c01a7cec039a4b
Sofia Strazzi <61978347+SofiaStrazzi@users.noreply.github.com>
2021-09-07 15:15:58 +0200

    Update MLpython.py

--- MLpython.py
+++ MLpython.py
@@ -254,7 +254,7 @@ print("\n\n--------------------------------------------------------------\n\n")
 
 print(" Creating an object for the predictions \n")
 predictions_data = lin_estimator.predict(data_input_fn)
-print(next(predictions_data))
+print(list(predictions_data))
 print("\n\n")
 
 
