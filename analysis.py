a9bbb5250bf404825f98b9359ac6b8669d10c5c7
Sofia Strazzi <61978347+SofiaStrazzi@users.noreply.github.com>
2021-09-07 16:31:35 +0200

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -3,15 +3,15 @@
 
 # The user can decide if use an analysis based on PYTHON libraries or based on ROOT TMVA
 
-print("\nYou can decide to use PYTHON libraries or TMVA to compare 4 different Machine Learning methodes and then use one of them to analyze the data\n")
-print("Press 1 to analyze data with PYTHON libraries or 2 to analyze them with TMVA \t")
+print("\nYou can decide to use the PYTHON script to see the performances of a linear estimator or the TMVA to compare 4 different Machine Learning methodes and then use one of them to analyze the data\n")
+print("Press 1 to use the PYTHON script or 2 to use ROOT TMVA \t")
 type_of_analysis = input()
 
 # Control on the input typed by the user:
 # if the answer is not consistent, I let ask the user to type a right command
 while (type_of_analysis != '1' and type_of_analysis != '2'):
 	print("\nERROR: the input doesn't corrispond to any option \n")
-	print("Press 1 to analyze data with Python libraries or 2 to analyze them with TMVA \t")
+	print("Press 1 to use the PYTHON script or 2 to use ROOT TMVA \t")
 	type_of_analysis = input()
 	
 import os
